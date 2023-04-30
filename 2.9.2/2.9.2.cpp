f6fe996d0f579ee8d9b32a94255bbb99fd8deb3d
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-04-30 22:57:09 +0300

    Second commit

--- 2.9.2/2.9.2.cpp
+++ 2.9.2/2.9.2.cpp
@@ -34,40 +34,47 @@ public:
 };
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	double n;
-	double m;
-	std::cout << "Введите числитель дроби 1: ";
-	std::cin >> n;
-	std::cout << "Введите знаменатель дроби 1: ";
-	std::cin >> m;
-	Fraction f1(n,m);
-	std::cout << "Введите числитель дроби 2: ";
-	std::cin >> n;
-	std::cout << "Введите знаменатель дроби 2: ";
-	std::cin >> m;
+	try {
+		setlocale(LC_ALL, "Russian");
+		double n;
+		double m;
+		std::cout << "Введите числитель дроби 1: ";
+		std::cin >> n;
+		std::cout << "Введите знаменатель дроби 1: ";
+		std::cin >> m;
+		if (m == 0) { throw std::exception("Деление на 0!"); }
+		Fraction f1(n, m);
+		std::cout << "Введите числитель дроби 2: ";
+		std::cin >> n;
+		std::cout << "Введите знаменатель дроби 2: ";
+		std::cin >> m;
+		if (m == 0) { throw std::exception("Деление на 0!"); }
 
-	Fraction f2(n, m);
-	Fraction f3 = f1 + f2;
-	Fraction f4 = f1 - f2;
-	Fraction f5 = f1 * f2;
-	Fraction f6 = f1 / f2;
-	Fraction f7 = (++f1) * f2;
-	Fraction f8 = (f1--) * f2;
+		Fraction f2(n, m);
+		Fraction f3 = f1 + f2;
+		Fraction f4 = f1 - f2;
+		Fraction f5 = f1 * f2;
+		Fraction f6 = f1 / f2;
+		Fraction f7 = (++f1) * f2;
+		Fraction f8 = (f1--) * f2;
 
-	f3.simplification();
-	f4.simplification();
-	f5.simplification();
-	f6.simplification();
-	f7.simplification();
-	f8.simplification();
+		f3.simplification();
+		f4.simplification();
+		f5.simplification();
+		f6.simplification();
+		f7.simplification();
+		f8.simplification();
 
-	std::cout << f1.get_num() << "/" << f1.get_den() << " + " << f2.get_num() << "/" << f2.get_den()  << " = " << f3.get_num() << "/" << f3.get_den() << std::endl;
-	std::cout << f1.get_num() << "/" << f1.get_den() << " - " << f2.get_num() << "/" << f2.get_den() << " = " << f4.get_num() << "/" << f4.get_den() << std::endl;
-	std::cout << f1.get_num() << "/" << f1.get_den() << " * " << f2.get_num() << "/" << f2.get_den() << " = " << f5.get_num() << "/" << f5.get_den() << std::endl;
-	std::cout << f1.get_num() << "/" << f1.get_den() << " / " << f2.get_num() << "/" << f2.get_den() << " = " << f6.get_num() << "/" << f6.get_den() << std::endl;
-	std::cout << "++" << f1.get_num() << "/" << f1.get_den() << " * " << f2.get_num() << "/" << f2.get_den() << " = " << f7.get_num() << "/" << f7.get_den() << std::endl;
-	std::cout << f1.get_num() << "/" << f1.get_den() << "-- * " << f2.get_num() << "/" << f2.get_den() << " = " << f8.get_num() << "/" << f8.get_den() << std::endl;
-	std::cout << "Значение дроби 1 = " << f1.get_num() << "/" << f1.get_den() << std::endl;
-	std::cout << "Значение дроби 2 = " << f2.get_num() << "/" << f2.get_den() << std::endl;
+		std::cout << f1.get_num() << "/" << f1.get_den() << " + " << f2.get_num() << "/" << f2.get_den() << " = " << f3.get_num() << "/" << f3.get_den() << std::endl;
+		std::cout << f1.get_num() << "/" << f1.get_den() << " - " << f2.get_num() << "/" << f2.get_den() << " = " << f4.get_num() << "/" << f4.get_den() << std::endl;
+		std::cout << f1.get_num() << "/" << f1.get_den() << " * " << f2.get_num() << "/" << f2.get_den() << " = " << f5.get_num() << "/" << f5.get_den() << std::endl;
+		std::cout << f1.get_num() << "/" << f1.get_den() << " / " << f2.get_num() << "/" << f2.get_den() << " = " << f6.get_num() << "/" << f6.get_den() << std::endl;
+		std::cout << "++" << f1.get_num() << "/" << f1.get_den() << " * " << f2.get_num() << "/" << f2.get_den() << " = " << f7.get_num() << "/" << f7.get_den() << std::endl;
+		std::cout << f1.get_num() << "/" << f1.get_den() << "-- * " << f2.get_num() << "/" << f2.get_den() << " = " << f8.get_num() << "/" << f8.get_den() << std::endl;
+		std::cout << "Значение дроби 1 = " << f1.get_num() << "/" << f1.get_den() << std::endl;
+		std::cout << "Значение дроби 2 = " << f2.get_num() << "/" << f2.get_den() << std::endl;
+	}
+	catch (std::exception str) {
+		std::cout << str.what() << std::endl;
+	}
 }
